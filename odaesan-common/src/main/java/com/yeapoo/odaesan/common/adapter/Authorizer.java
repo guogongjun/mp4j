68e2b11438ff68fecb3dcb0a75457fe21272df33
SimonDolph <simon.dong.sea@hotmail.com>
2014-09-09 11:38:26 +0800

    解决access token的奇怪问题

--- odaesan-common/src/main/java/com/yeapoo/odaesan/common/adapter/Authorizer.java
+++ odaesan-common/src/main/java/com/yeapoo/odaesan/common/adapter/Authorizer.java
@@ -3,6 +3,8 @@ package com.yeapoo.odaesan.common.adapter;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -12,6 +14,7 @@ import com.yeapoo.odaesan.sdk.model.Authorization;
 
 @Component
 public class Authorizer {
+    private static Logger logger = LoggerFactory.getLogger(Authorizer.class);
 
     @Autowired
     private AuthClient authClient;
@@ -30,14 +33,12 @@ public class Authorizer {
     public synchronized Authorization authorize(Map<String, Object> appInfo) {
         String infoId = MapUtil.get(appInfo, "id");
         Authorization auth = cache.get(infoId);
-        if (null != auth && !auth.isExpired()) {
-            return auth;
-        } else {
-            String appId = MapUtil.get(appInfo, "app_id");
-            String appSecret = MapUtil.get(appInfo, "app_secret");
-            auth = authClient.requestAuthorization(appId, appSecret);
-            cache.put(infoId, auth);
-            return auth;
-        }
+        logger.info(String.format("access token [%s], request at [%s], expected expires in [%s]", auth.getAccessToken(), auth.getLastRequestTime(), auth.getExpiresIn()));
+
+        String appId = MapUtil.get(appInfo, "app_id");
+        String appSecret = MapUtil.get(appInfo, "app_secret");
+        auth = authClient.requestAuthorization(appId, appSecret);
+        cache.put(infoId, auth);
+        return auth;
     }
 }
